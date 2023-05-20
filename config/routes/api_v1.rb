91529551c65199e83279d59bd468f5981354f441
Anurag Upadhyay <anuragle100@gmail.com>
2023-05-20 11:53:08 +0530

    added fix for cors issue

--- config/routes/api_v1.rb
+++ config/routes/api_v1.rb
@@ -1,22 +1,16 @@
 # frozen_string_literal: true
 
-scope :accounts do
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: %i[create index show] do
-        collection do
-          post 'verify'
-          post 'login'
-          post 'login_otp'
-          post 'otp_verification'
-          get 'registered'
-        end
-      end
-    end
-  end
-end
 namespace :api do
   namespace :v1 do
+    resources :users, only: %i[create index show] do
+      collection do
+        post 'verify'
+        post 'login'
+        post 'login_otp'
+        post 'otp_verification'
+        get 'registered'
+      end
+    end
     resources :file_upload, only: %i[create]
     resources :profiles do
       collection do
