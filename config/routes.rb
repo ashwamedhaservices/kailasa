94bc8f91de900ddd37db09e000b3c9736804d4b4
Naveen Kumar H S <naveen.kumar@kuvera.in>
2023-05-22 09:17:50 +0530

    health check API added
    Signed-off-by: Naveen Kumar H S <naveen.kumar@kuvera.in>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/health_check', to: proc { [200, {}, ['success']] }
   draw :api_v1
   draw :admin_api_v1
   draw :interface_api_v1
