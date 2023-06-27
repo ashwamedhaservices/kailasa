f94f4398e608634790f3aeb97636e7ad262e712c
Anurag Upadhyay <anuragle100@gmail.com>
2023-06-28 00:05:04 +0530

    add referral credit

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -24,6 +24,7 @@ module Api
           current_user = data.user
           current_user.update(subscribed: true)
           Subscriptions::Purchase.call(current_user, :one_year)
+          ::Referral::Credit::Processor.call(current_user) if current_user.referrer_id
           return redirect_to 'https://www.ashwamedha.net/payments/payu/response?status=success', allow_other_host: true
         end
 
