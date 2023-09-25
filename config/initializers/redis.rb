e2fc81537ded44f6fdc2c7c895e7e2e3cff1d362
Anurag Upadhyay <anuragle100@gmail.com>
2023-09-26 00:29:51 +0530

    added db and username

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,9 @@
 
 REDIS = ConnectionPool::Wrapper.new(size: Rails.configuration.redis.pool_size,
                                     timeout: Rails.configuration.redis.timeout) do
-  Redis.new(host: Rails.configuration.redis.host, port: Rails.configuration.redis.port,
+  Redis.new(username: Rails.configuration.redis.username,
+            host: Rails.configuration.redis.host,
+            port: Rails.configuration.redis.port,
+            db: Rails.configuration.redis.db,
             ssl: Rails.configuration.redis.ssl)
 end
